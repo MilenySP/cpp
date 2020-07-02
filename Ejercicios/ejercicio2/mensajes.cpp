b8b13fa064b75383502d394233ff42ee9bb1a838
Mileny Pineda <milenysoad@gmail.com>
2020-07-02 04:10:23 -0600

    agregando ejercicio 2

---
+++ Ejercicios/ejercicio2/mensajes.cpp
@@ -0,0 +1,14 @@
+# include  <iostream>
+using namespace std;
+
+int  main ( int argc, char  const * argv [])
+{
+    string nombre;
+    cout<< " INGRESE SU NOMBRE: " ;
+    cin >> nombre;
+    cout << endl;
+
+    cout << " HOLA MI NOMBRE ES: " << nombre;
+    cout << endl;
+    return  0 ;
+}
