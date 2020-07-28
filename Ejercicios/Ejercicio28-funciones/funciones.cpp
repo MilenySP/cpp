80ff244e51c825f31712981056bf12d5125e1607
Mileny Pineda <milenysoad@gmail.com>
2020-07-28 08:55:11 -0600

    ejercicios

---
+++ Ejercicios/Ejercicio28-funciones/funciones.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int suma ()
+{
+    return 5 + 7;
+}
+
+string nombrecompleto()
+{
+    string nombre = "Juan";
+    string apellido = " Perez";
+    return "Juan Perez";
+}
+
+int main(int argc, char const *argv[])
+{
+    int resultado = suma();
+    string nombreyapellido = nombrecompleto();
+    cout << resultado;
+
+    cout << endl;
+    cout << nombreyapellido;
+
+    return 0;
+}
