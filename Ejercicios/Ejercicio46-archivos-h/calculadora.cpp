6e343057fa5f5b3a94e006fe0531948f6e4e0125
Mileny Pineda <milenysoad@gmail.com>
2020-08-09 16:38:02 -0600

    examen y ejercicio

---
+++ Ejercicios/Ejercicio46-archivos-h/calculadora.cpp
@@ -0,0 +1,16 @@
+int sumar (int a, int b)
+{
+    return a + b;
+} 
+int restar (int a, int b)
+{
+    return a - b;
+}
+int calcular(int a, int b, char operador)
+{
+    if (operador == '+')
+    {
+    return sumar(a, b);  
+    }
+    throw "operdor no valido";
+}
